3af1704dbb3cfe528a8d68f41671d56bbaf399ff
Jamie Snell <jgsnell@gmail.com>
2016-01-18 19:16:30 -0600

    add zip installer

--- CLRProfiler/tools/chocolateyinstall.ps1
+++ CLRProfiler/tools/chocolateyinstall.ps1
@@ -1,61 +1,9 @@
-﻿# IMPORTANT: Before releasing this package, copy/paste the next 2 lines into PowerShell to remove all comments from this file:
-#   $f='c:\path\to\thisFile.ps1'
-#   gc $f | ? {$_ -notmatch "^\s*#"} | % {$_ -replace '(^.*?)\s*?[^``]#.*','$1'} | Out-File $f+".~" -en utf8; mv -fo $f+".~" $f
+﻿
+$ErrorActionPreference = 'Stop';
 
-$ErrorActionPreference = 'Stop'; # stop on all errors
-
-$packageName= 'CLR Profiler' # arbitrary name for the package, used in messages
+$packageName= 'CLR Profiler'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://clrprofiler.codeplex.com/downloads/get/532810' # download url
+$url        = 'https://clrprofiler.codeplex.com/downloads/get/532810'
+$checksum   = 'E024DFEEDFE19B5BC1198BA34E14B9A7'
 
-$packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = 'exe'
-  url           = $url
-  url64bit      = $url64
-  #file         = $fileLocation
-
-  #MSI
-  silentArgs    = "/qn /norestart /l*v `"$env:TEMP\chocolatey\$($packageName)\$($packageName).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
-  validExitCodes= @(0, 3010, 1641)
-  #OTHERS
-  # Uncomment matching EXE type (sorted by most to least common)
-  silentArgs   = "/Q /T:`"$env:TEMP\chocolatey\$($packageName)`""
-
-  # optional, highly recommended
-  softwareName  = 'CLRProfiler*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-  checksum      = ''
-  checksumType  = 'md5' #default is md5, can also be sha1
-  checksum64    = ''
-  checksumType64= 'md5' #default is checksumType
-}
-
-Install-ChocolateyPackage @packageArgs
-#Install-ChocolateyZipPackage @packageArgs
-# if you are making your own internal packages (organizations), you can embed the installer or 
-# put on internal file share and use the following instead (you'll need to add $file to the above)
-#Install-ChocolateyInstallPackage @packageArgs 
-
-## Main helper functions - these have error handling tucked into them already
-## see https://github.com/chocolatey/choco/wiki/HelpersReference
-
-## Install an application, will assert administrative rights
-## add additional optional arguments as necessary
-##Install-ChocolateyPackage $packageName $fileType $silentArgs $url [$url64 -validExitCodes $validExitCodes -checksum $checksum -checksumType $checksumType -checksum64 $checksum64 -checksumType64 $checksumType64]
-
-## Download and unpack a zip file
-Install-ChocolateyZipPackage $packageName $url $toolsDir [$url64 -checksum $checksum -checksumType $checksumType -checksum64 $checksum64 -checksumType64 $checksumType64]
-
-# unzips a file to the specified location - auto overwrites existing content
-#Get-ChocolateyUnzip "FULL_LOCATION_TO_ZIP.zip" $toolsDir
-
-# Runs processes asserting UAC, will assert administrative rights - used by Install-ChocolateyInstallPackage
-#Start-ChocolateyProcessAsAdmin 'STATEMENTS_TO_RUN' 'Optional_Application_If_Not_PowerShell' -validExitCodes $validExitCodes
-
-# add specific folders to the path - any executables found in the chocolatey package folder will already be on the path. This is used in addition to that or for cases when a native installer doesn't add things to the path.
-#Install-ChocolateyPath 'LOCATION_TO_ADD_TO_PATH' 'User_OR_Machine' # Machine will assert administrative rights
-
-# add specific files as shortcuts to the desktop
-#$target = Join-Path $toolsDir "$($packageName).exe"
-# Install-ChocolateyShortcut -shortcutFilePath "<path>" -targetPath "<path>" [-workDirectory "C:\" -arguments "C:\test.txt" -iconLocation "C:\test.ico" -description "This is the description"]
+Install-ChocolateyZipPackage 'clrprofiler' $url $toolsDir -checksum $checksum
