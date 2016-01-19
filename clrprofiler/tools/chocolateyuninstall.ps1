c7e54e1744562f3e93d1fb914de563af282b5e8d
Jamie Snell <jamie.snell@hudl.com>
2016-01-18 20:30:16 -0600

    Update chocolateyuninstall.ps1

--- clrprofiler/tools/chocolateyuninstall.ps1
+++ clrprofiler/tools/chocolateyuninstall.ps1
@@ -3,5 +3,5 @@
 $packageName = 'clrprofiler'
 $softwareName = 'CLRProfiler*'
 $zipfile = "CLRProfiler45Binaries.zip"
-#Uninstall-ChocolateyZipPackage $packageName $zipfile
+Uninstall-ChocolateyZipPackage $packageName $zipfile
 
